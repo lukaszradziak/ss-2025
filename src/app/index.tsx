e27d8c8b8168e4c2fe18df4b8820efaf9ca344ec
Łukasz Radziak <lukasz.radziak@icloud.com>
2025-03-11 19:29:28 +0100

    Init project.

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <div style={{
+        margin: "20vh auto",
+        width: "fit-content",
+        height: "40vh",
+        padding: "0",
+        display: "flex",
+      }}>
+        <img src="logo-skysnap-sq.webp" />
+      </div>
+      <h1 style={{ 
+        textAlign: "center",
+        fontFamily: "sans-serif, Arial",
+      }}>SkySnap - Rekrutacja 2025</h1>
+    </>
+  )
+}
+
+export default App
